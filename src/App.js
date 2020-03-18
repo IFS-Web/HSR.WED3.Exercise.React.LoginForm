9ca1a76790aa9a5bbcf2a1fc5bfaa7eeef4668b3
Mirko Stocker <misto@Mirkos-MBP.home>
2020-03-18 15:45:26 +0100

    Simple form, no handling yet.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const showNoAtInEmail = false;
+    const showPasswordTooShort = false;
+    return (
+      <div className="App">
+        <form>
+          <label htmlFor="email">E-Mail</label>
+          <br />
+          <input type="text" name="email" />
+          <p
+            style={{ visibility: showNoAtInEmail ? "visible" : "hidden" }}
+            className="hint"
+          >
+            Achtung, die Adresse enthält kein @.
+          </p>
+          <label htmlFor="password">Password</label>
+          <br />
+          <input type="password" name="password" />
+          <p
+            style={{ visibility: showPasswordTooShort ? "visible" : "hidden" }}
+            className="hint"
+          >
+            Das Passwort muss mindestens 8 Zeichen lang sein.
+          </p>
+          <button type="submit">Log-in</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
