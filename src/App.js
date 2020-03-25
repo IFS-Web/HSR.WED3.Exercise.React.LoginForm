200bff91c738d2457680494f19b771fcfe91095d
Mirko Stocker <misto@Mirkos-MBP.home>
2020-03-25 09:00:43 +0100

    Solved it!

--- src/App.js
+++ src/App.js
@@ -2,15 +2,46 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  state = {
+    email: "",
+    password: "",
+    emailEdited: false,
+    passwordEdited: false
+  };
+
+  handleEmailChange = event => {
+    this.setState({ email: event.target.value });
+  };
+
+  handlePasswordChange = event => {
+    this.setState({ password: event.target.value });
+  };
+
+  handleEmailEdit = () => {
+    this.setState({ emailEdited: true });
+  };
+
+  handlePasswordEdit = () => {
+    this.setState({ passwordEdited: true });
+  };
+
   render() {
-    const showNoAtInEmail = false;
-    const showPasswordTooShort = false;
+    const showNoAtInEmail =
+      this.state.emailEdited && this.state.email.indexOf("@") === -1;
+    const showPasswordTooShort =
+      this.state.passwordEdited && this.state.password.length < 8;
     return (
       <div className="App">
         <form>
           <label htmlFor="email">E-Mail</label>
           <br />
-          <input type="text" name="email" />
+          <input
+            type="text"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleEmailChange}
+            onBlur={this.handleEmailEdit}
+          />
           <p
             style={{ visibility: showNoAtInEmail ? "visible" : "hidden" }}
             className="hint"
@@ -19,7 +50,13 @@ class App extends React.Component {
           </p>
           <label htmlFor="password">Password</label>
           <br />
-          <input type="password" name="password" />
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+            onBlur={this.handlePasswordEdit}
+          />
           <p
             style={{ visibility: showPasswordTooShort ? "visible" : "hidden" }}
             className="hint"
